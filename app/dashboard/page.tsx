2b0040702bd5b49b59b921333dbf4a4fc90f2542
Rohan Kanchan Saxena <rohankanchan2844@gmail.com>
2026-09-24 01:22:47 +0530

    Dashboard Layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div>
+      <UserButton/>
+      
+    </div>
+  )
+}
+
+export default DashboardPage
